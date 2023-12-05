2d639a786bc316446b86da62b3b9971627ab6853
Jorge Abarca <jorgeabarca92@gmail.com>
2023-12-04 20:30:29 -0600

    added new file datatypes.py

---
+++ day02/datatypes.py
@@ -0,0 +1,28 @@
+#Data Types
+
+#String
+
+"Hello"
+
+#Find characters of string:
+
+print('Hello'[0]) #we get H
+
+#To get last letter we use:
+
+print('Hello'[-1])
+
+#Integer - Whole numbers
+
+123 + 345
+123_456_789
+
+#Float - numbers with decimal points
+
+3.14
+12121.122
+
+#Boolean
+
+True
+False
