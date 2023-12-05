d05a9611c54dff673dd17baaf7543d3508896ea3
Jorge Abarca <jorgeabarca92@gmail.com>
2023-12-05 09:51:55 -0600

    added life in weeks app

---
+++ day02/life_in_weeks.py
@@ -0,0 +1,11 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+
+age = int(age)
+age_in_weeks = age * 52
+ninety_to_weeks = 90 * 52
+weeks_left = ninety_to_weeks - age_in_weeks
+print(f"You have {weeks_left} weeks left.")
+
+
