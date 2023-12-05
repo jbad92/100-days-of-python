79fd46223d7492e764f60e04fb60068532f092fa
Jorge Abarca <jorgeabarca92@gmail.com>
2023-12-05 11:47:42 -0600

    added solution to challenge

---
+++ day03/odd_or_even.py
@@ -0,0 +1,10 @@
+# Which number do you want to check?
+number = int(input())
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+if number % 2 == 0:
+    print('Number is even')
+else:
+    print('Number is odd')
+    
